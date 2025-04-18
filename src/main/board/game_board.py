0f82e315b0f8fd21046f82b33a576326d48592ca
Timo <timo.e.gebhardt@gmail.com>
2025-04-18 18:57:39 +0200

    Ermögliche die Eingabe der Schiffe

--- src/main/board/game_board.py
+++ src/main/board/game_board.py
@@ -1,3 +1,17 @@
 from numpy import *     # Für Matrix notwendig
+
+WASSER = "🌊"
+SCHIFF = "🚢"
+TREFFER = "🔥"
+FEHLSCHUSS = "❌"
+
 board1 = array([[None] * 10] * 10)   # 10 x 10 Matrix
 board2 = array([[None] * 10] * 10)   # 10 x 10 Matrix
+
+def initialisiere_board():
+    return array([[WASSER] * 10] * 10)
+
+def zeige_board(board, verdeckt):
+    # TODO: gibt das Board mit Rahmen und Zeilen- (A-J) und Spalten- (1-10) Bezeichungen aus
+    # Wenn "verdeckt", dann sollen die Schiffe als Wasser gezeigt werden
+    print(board)
--- src/main/board/ship.py
+++ src/main/board/ship.py
@@ -1,1 +1,139 @@
-from game_board import *
+from board.game_board import *
+
+OBEN = 1
+RECHTS = 2
+LINKS = 3
+UNTEN = 4
+
+Schifflängen = [5,4,3,3,2]
+
+def setze_schiffe(board):
+    for schifflänge in Schifflängen:
+        setze_nächstes_schiff(board, schifflänge)
+
+
+def setze_nächstes_schiff(board, schifflänge: int):
+
+    isStartPosMöglich = False
+    while not isStartPosMöglich:
+        koordinate = input("Nenne deine Koordinate für das Schiff mit der Länge " + str(schifflänge) + ": ")
+
+        x,y = dekodiere_koordinate(koordinate)
+
+        if kann_schiff_gesetzt_werden(board, x, y, OBEN, schifflänge):
+            isStartPosMöglich = True
+            print(str(OBEN) + ": OBEN")
+        if kann_schiff_gesetzt_werden(board, x, y, RECHTS, schifflänge):
+            isStartPosMöglich = True
+            print(str(RECHTS) + ": RECHTS")
+        if kann_schiff_gesetzt_werden(board, x, y, LINKS, schifflänge):
+            isStartPosMöglich = True
+            print(str(LINKS) + ": LINKS")
+        if kann_schiff_gesetzt_werden(board, x, y, UNTEN, schifflänge):
+            isStartPosMöglich = True
+            print(str(UNTEN) + ": UNTEN")
+
+        if isStartPosMöglich:
+            richtung = lese_int(1, 4, "In welche Richtung soll das Schiff zeigen? ")
+        else:
+            print("Hier kann kein Schiff platziert werden.")
+
+    setze_schiff_in_board(board, x, y, richtung, schifflänge)
+    zeige_board(board, False)
+
+def kann_schiff_gesetzt_werden(board, x : int, y : int, richtung, schifflänge : int) -> bool:
+    if x > 9 or x < 0 or y > 9 or y < 0:
+        return False
+    if richtung == LINKS:
+        endpunkty = y - (schifflänge - 1)
+        if endpunkty < 0:
+            return False
+        for i in range(schifflänge):
+            if board[x][y-i] == SCHIFF:
+                return False
+    elif richtung == UNTEN:
+        endpunktx = x + schifflänge - 1
+        if endpunktx > 9:
+            return False
+        for i in range(schifflänge):
+            if board[x+i][y] == SCHIFF:
+                return False
+    elif richtung == RECHTS:
+        endpunkty = y + schifflänge - 1
+        if endpunkty > 9:
+            return False
+        for i in range(schifflänge):
+            if board[x][y+1] == SCHIFF:
+                return False
+    elif richtung == OBEN:
+        endpunktx = x - (schifflänge - 1)
+        if endpunktx < 0:
+            return False
+        for i in range(schifflänge):
+            if board[x-i][y] == SCHIFF:
+                return False
+    return True
+
+def setze_schiff_in_board(board, x : int, y : int, richtung: int, schifflänge : int):
+    if richtung == LINKS:
+        for i in range(schifflänge):
+            board[x][y-i] = SCHIFF
+    elif richtung == UNTEN:
+        for i in range(schifflänge):
+            board[x+i][y] = SCHIFF
+    elif richtung == RECHTS:
+        for i in range(schifflänge):
+            board[x][y+i] = SCHIFF
+    elif richtung == OBEN:
+        for i in range(schifflänge):
+            board[x-i][y] = SCHIFF
+
+
+
+def dekodiere_koordinate(koordinatenString):
+    x = koordinatenString[0]
+    y = koordinatenString[1:3]
+    y = int(y)
+    y = y-1
+    x = x.upper()
+
+    if x == "A":
+        x = 0
+    elif x == "B":
+        x = 1
+    elif x == "C":
+        x = 2
+    elif x == "D":
+        x = 3
+    elif x == "E":
+        x = 4
+    elif x == "F":
+        x = 5
+    elif x == "G":
+        x = 6
+    elif x == "H":
+        x = 7
+    elif x == "I":
+        x = 8
+    elif x == "J":
+        x = 9
+    else:
+        x = -1
+
+    x = int(x)
+    y = int(y)
+    return x,y
+
+
+def lese_int(erlaubterMinWert: int, erlaubterMaxWert: int, aufforderungsText: str) -> int:
+    while True:
+        eingabe = input(aufforderungsText)
+        try:
+            wert = int(eingabe)
+            if erlaubterMinWert <= wert and wert <= erlaubterMaxWert:
+                return wert
+            else:
+                print("Bitte eine Zahl zwischen " + erlaubterMinWert + " und " + erlaubterMaxWert + "eingeben.")
+        except ValueError:
+            print("Das war keine gültige Zahl.")
+
